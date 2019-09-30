d09498e5c54afe6513d6532ced88dbc2a7486cc1
nefeltellioglu <30437020+nefeltellioglu@users.noreply.github.com>
2019-09-30 21:10:53 +0300

    Add files via upload

--- files/Datasets.R
+++ files/Datasets.R
@@ -80,8 +80,160 @@ source("Polarity Discovery Code.R")
   
 }
 
+#Dataset2 
+{
+  
+  #effect functions
+  {
+    plot(x1,((15/(1+exp(-0.4*(x1-x1[1]))))-2)/7.5)
+    plot(x2,(x2/20+0.75))
+    plot(x3, (14-(20/(1+exp(-0.1*(x3-x3[1])))))/13.77+0.3)
+    
+    x1<-runif(3000,min(x1),max(x1))
+    x2<-runif(3000,min(x2),max(x2))
+    x3<- runif(3000,min(x3),max(x3))
+    
+    y<- 100*(((15/(1+exp(-0.4*(x1-x1[1]))))-2)/7.5)*(x2/20+0.75)*((14-(20/(1+exp(-0.1*(x3-x3[1])))))/13.77+0.3)
+    
+    datx1<-data.frame(x1=x1[1:3000], fx1=(((15/(1+exp(-0.4*(x1-x1[1]))))-2)/7.5)[1:3000])
+    datx2<-data.frame(x2=x2[1:3000], fx2=(x2/20+0.75)[1:3000])
+    datx3<-data.frame(x3=x3[1:3000], fx3=((14-(20/(1+exp(-0.1*(x3-x3[1])))))/13.77+0.3 )[1:3000])
+    
+    
+    ggplot(datx1) + 
+      aes(x = x1, y = fx1, group="l")+ geom_line(size=2)+
+      theme_bw()+
+      theme(plot.title=element_text(size=16,face="bold"),
+            axis.text=element_text(size=14),
+            axis.title=element_text(size=16))+
+      labs(x="X1", y = "f1(X1)")+ 
+      ggtitle("Effect of X1 on Y") +theme(plot.title = element_text(hjust = 0.5))
+    
+    ggplot(datx2) + theme_bw()+
+      aes(x = x2, y = fx2, group="l")+ geom_line(size=2)+
+      theme(plot.title=element_text(size=16,face="bold"),
+            axis.text=element_text(size=14),
+            axis.title=element_text(size=16))+
+      labs(x="X2", y = "f2(X2)")+ 
+      ggtitle("Effect of X2 on Y") +theme(plot.title = element_text(hjust = 0.5)) #+
+    
+    ggplot(datx3) + theme_bw()+
+      aes(x = x3, y = fx3, group="l")+ geom_line(size=2)+
+      theme(plot.title=element_text(size=16,face="bold"),
+            axis.text=element_text(size=14),
+            axis.title=element_text(size=16))+
+      labs(x="X3", y = "f3(X3)")+ 
+      ggtitle("Effect of X3 on Y") +theme(plot.title = element_text(hjust = 0.5)) 
+    
+  }
+  x1<- c(seq(5,14,length.out =80), seq(13,7,length.out =10), seq(7,9,length.out =10))
+  x2<- c(seq(11,4,length.out =50),seq(4,7,length.out =20),seq(7,1,length.out =30) )
+  k<-c(seq(60,10,length.out =100) )
+  x3<-50/(1+exp(-0.07*(k-k[1])))
+  
+  ind<-data.frame(cbind(x1,x2,x3))
+  cor(ind)
+  y<- 100*(((15/(1+exp(-0.4*(x1-x1[1]))))-2)/7.5)*(x2/20+0.75)*((14-(20/(1+exp(-0.1*(x3-x3[1])))))/13.77+0.3)
+  all<-data.frame(cbind(x1,x2,x3,y))
+  m<-as.matrix(cor(all, method=c("spearman")))
+  m
+  xtable(m, digit=4)
+  
+  #dataset plot
+  {
+    
+    datx1<-data.frame(X1=x1, Y=y, X2=x2, X3=x3)
+    
+    ggplot(datx1) + 
+      aes( x= seq(1, length(X1)),y = X1, group="l")+ geom_point(size=1)+
+      theme_bw()+
+      theme(plot.title=element_text(size=16,face="bold"),
+            axis.text=element_text(size=14),
+            axis.title=element_text(size=16))+
+      labs(x="Time", y = "")+ 
+      ggtitle("X1 Variable") +theme(plot.title = element_text(hjust = 0.5))
+    
+    ggplot(datx1) + 
+      aes( x= seq(1, length(X2)),y = X2, group="l")+ geom_point(size=1)+
+      theme_bw()+
+      theme(plot.title=element_text(size=16,face="bold"),
+            axis.text=element_text(size=14),
+            axis.title=element_text(size=16))+
+      labs(x="Time", y = "")+ 
+      ggtitle("X2 Variable") +theme(plot.title = element_text(hjust = 0.5))
+    
+    ggplot(datx1) + 
+      aes( x= seq(1, length(X3)),y = X3, group="l")+ geom_point(size=1)+
+      theme_bw()+
+      theme(plot.title=element_text(size=16,face="bold"),
+            axis.text=element_text(size=14),
+            axis.title=element_text(size=16))+
+      labs(x="Time", y = "")+ 
+      ggtitle("X3 Variable") +theme(plot.title = element_text(hjust = 0.5))
+    
+    ggplot(datx1) + 
+      aes( x= seq(1, length(Y)),y = Y, group="l")+ geom_point(size=1)+
+      theme_bw()+
+      theme(plot.title=element_text(size=16,face="bold"),
+            axis.text=element_text(size=14),
+            axis.title=element_text(size=16))+
+      labs(x="Time", y = "")+ 
+      ggtitle("Y Variable") +theme(plot.title = element_text(hjust = 0.5))
+    
+  }
+  #dataset cross sectional plot
+  {
+    
+    datx1<-data.frame(X1=x1, Y=y, X2=x2, X3=x3)
+    
+    ggplot(datx1) + 
+      aes( x= X1,y = Y, group="l")+ geom_point(size=1)+
+      theme_bw()+
+      theme(plot.title=element_text(size=16,face="bold"),
+            axis.text=element_text(size=14),
+            axis.title=element_text(size=16))+
+      labs(x="X1", y = "Y")+ 
+      ggtitle("X1 vs Y") +theme(plot.title = element_text(hjust = 0.5))
+    
+    ggplot(datx1) + 
+      aes( x= X2,y = Y, group="l")+ geom_point(size=1)+
+      theme_bw()+
+      theme(plot.title=element_text(size=16,face="bold"),
+            axis.text=element_text(size=14),
+            axis.title=element_text(size=16))+
+      labs(x="X2", y = "Y")+ 
+      ggtitle("X2 vs Y") +theme(plot.title = element_text(hjust = 0.5))
+    
+    ggplot(datx1) + 
+      aes( x= X3,y = Y, group="l")+ geom_point(size=1)+
+      theme_bw()+
+      theme(plot.title=element_text(size=16,face="bold"),
+            axis.text=element_text(size=14),
+            axis.title=element_text(size=16))+
+      labs(x="X3", y = "Y")+ 
+      ggtitle("X3 vs Y") +theme(plot.title = element_text(hjust = 0.5))
+    
+  }
+  limits<-t(sapply(1:ncol(ind), function(x) c(Min=min(ind[,x]), Max=max(ind[,x]))))
+  limits[1,2]<-12.5
+  
+  discoverpolarity(causes=ind,effect =y,threshold1=0.05,limits=limits)
+  
+  
+  
+  c(sign(pcor.test(x1,y, c(x2,x3), method="spearman")$estimate),
+    sign(pcor.test(x2,y, c(x1,x3), method="spearman")$estimate),
+    sign(pcor.test(x3,y, c(x1,x2), method="spearman")$estimate) )
+  c(pcor.test(x1,y, c(x2,x3), method="spearman")$estimate,
+    pcor.test(x2,y, c(x1,x3), method="spearman")$estimate,
+    pcor.test(x3,y, c(x1,x2), method="spearman")$estimate )
+  c((pcor.test(x1,y, c(x2,x3), method="spearman")$p.value),
+    (pcor.test(x2,y, c(x1,x3), method="spearman")$p.value),
+    (pcor.test(x3,y, c(x1,x2), method="spearman")$p.value))
+  
+}
 
-#Dataset2
+#Dataset3
 {
   #effect functions
   {
@@ -219,8 +371,7 @@ source("Polarity Discovery Code.R")
   
 }
 
-
-#Dataset3
+#Dataset4
 {
   
   #effect functions
